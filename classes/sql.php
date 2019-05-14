e89f037187964b46230563dd6914555d1d4c880a
vmuravyev <wladimir.muravyev@gmail.com>
2019-05-14 18:17:12 +0300

    Velar 1

---
+++ classes/sql.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wladi
+ * Date: 13.05.2019
+ * Time: 19:49
+ */
+
+class sql
+{
+
+}
---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+require_once __DIR__ . "/classes/files.php";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Range Rover Velar | The Avant-garde Range Rover</title>
+    <meta name="keywords" content="Land Rover, Jaguar Land Rover, Range Rover Velar,
+    Range Rover, Velar, crossover, SUV, luxury SUV, Range Rover Velar price, Range Rover Velar range,
+    Range Rover Velar 0-100, performance, all wheel drive, PHEV, hybrid">
+    <meta name="description" content="Range Rover Velar is our most refined and capable medium SUV.
+    Explore its advanced driving capabilities and stunning design in detail.">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta content="True" name="HandheldFriendly">
+    <meta id="viewport-tag" name="viewport" content="width=device-width, user-scalable=no,
+    minimum-scale=1.0, maximum-scale=1.0, initial-scale=1, minimal-ui, shrink-to-fit=no">
+    <link rel="icon" type="image/png" href="/img/favicon.png">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="/img/mstile.png">
+    <link rel="stylesheet" href="<?= Files::noCash('/style.css') ?>"
+    " type="text/css">
+    <script type="text/javascript" src="/js/jquery.js"></script>
+
+</head>
+<body>
+<div class="container b1">
+    <nav>
+        <ul class="topmenu">
+            <li><a href="#" <b>Range Rover</b></a></li>
+            <li><a href="#" <b>Sport</b></a></li>
+            <li><a href="#" <b>Velar</b></a></li>
+            <li><a href="#" <b>Evoque</b></a></li>
+            <li><a href="#" <b>PHEV</b></a></li>
+        </ul>
+    </nav>
+    <nav>
+        <ul class="topmenu top-left-menu">
+            <li><a href="#" <b>Shop</b></a></li>
+            <li><a href="#" <b>Sign in</b></a></li>
+    </nav>
+    <div class="logo">
+    </div>
+</div>
+<div class="container b2">
+</div>
+<div class="container b3">
+</div>
+<div class="container b5">
+</div>
+<div class="container b6">
+</div>
+<div class="container b7">
+</div>
+<div class="container b8">
+</div>
+<div class="container b9">
+</div>
+<div class="container b10">
+</div>
+</body>
+</html>
